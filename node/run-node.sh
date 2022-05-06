f881966dc65691e582686231b92a0dbf14c9228b
root <jterrier84@gmail.com>
2022-05-06 21:01:02 +0200

    Add NODE SOCKET env varibale in docker image

--- node/run-node.sh
+++ node/run-node.sh
@@ -12,11 +12,11 @@ CNPROMETHEUS_PORT="12799"                                        ## Define the p
 CN_CONFIG_PATH="/home/julienterrier/Cardano-node-docker/node/files" ## Path to the folder where the Cardano config files are stored on the host system
 CN_DB_PATH="/home/julienterrier/Cardano-node-docker/node/db"     ## Path to the folder where the Cardano database (blockchain) will be stored on the host system
 CN_RTS_OPTS="+RTS -N2 -I0.1 -Iw3600 -A64m -AL128M -n4m -F1.1 -H3500M -O3500M -RTS"      ## RTS optimization parameters
-CN_BF_ID="mainnetd9PBzlK7KB7wWko8NTKUwJIsHfvEKNaV"               ## Your blockfrost.io project ID (for ScheduledBlock script)
-CN_POOL_ID="c3e7025ebae638e994c149e5703e82619b31897c9e1d64fc684f81c2"   ## Your stake pool ID (for ScheduledBlock script)
-CN_POOL_TICKER="MINI1"                                           ## Your pool ticker (for ScheduledBlock script)
-CN_VRF_SKEY_PATH="scheduledblocks.vrf.skey"                      ## Name of the vrf.skey file. It must be located in the same directory as CN_KEY_PATH (for ScheduledBlock script)
-CN_KEY_PATH="/home/julienterrier/Cardano-node-docker/node/files/.keys"  ## Path to the folder where the OP certificate and keys are stored on the host system
+CN_BF_ID=""                                                      ## Your blockfrost.io project ID (for ScheduledBlock script)
+CN_POOL_ID=""                                                    ## Your stake pool ID (for ScheduledBlock script)
+CN_POOL_TICKER=""                                                ## Your pool ticker (for ScheduledBlock script)
+CN_VRF_SKEY_PATH=""                                              ## Name of the vrf.skey file. It must be located in the same directory as CN_KEY_PATH (for ScheduledBlock script)
+CN_KEY_PATH=""                                                   ## Path to the folder where the OP certificate and keys are stored on the host system
 
 
 ##Do not edit/change section below!
@@ -35,6 +35,7 @@ docker run --detach \
     -e POOLID=${CN_POOL_ID} \
     -e POOLTICKER=${CN_POOL_TICKER} \
     -e SB_VRF_SKEY_PATH=${CN_VRF_SKEY_PATH} \
+    -e CARDANO_NODE_SOCKET_PATH=/home/cardano/pi-pool/db/node.socket \
     -v ${CN_CONFIG_PATH}:/home/cardano/pi-pool/files  \
     -v ${CN_DB_PATH}:/home/cardano/pi-pool/db \
     -v ${CN_KEY_PATH}:/home/cardano/pi-pool/.keys \
